a5f29a42dbb56974a57470e04aa976feb3ec736f
2semClab <158600040+2semClab@users.noreply.github.com>
2024-04-01 18:08:21 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -9,11 +9,9 @@ int main(){
     scanf("%d",&col);
     int a[row][col];
     printf("Enter the elements of the matrix (row major order) : ");
-    for(int i = 0 ; i<row;i++){
-        for(int j = 0; j<col;j++){
+    for(int i = 0 ; i<row;i++)
+        for(int j = 0; j<col;j++)
             scanf("%d",&a[i][j]);
-        }
-    }
     printf("Given Matrix : \n");
     for(int i =0; i<row;i++){
         for(int j = 0;j<col;j++){
@@ -34,11 +32,9 @@ int main(){
 void transpose(int *a, int row, int col){
     for(int i =0;i<row;i++){
         for(int j =i+1 ; j<col; j++){
-            
                 int temp = *((a+i*col)+j);
                 *((a+i*col)+j) = *((a+j*col)+i);
                 *((a+j*col)+i) = temp;
         }
     }
-
 }
